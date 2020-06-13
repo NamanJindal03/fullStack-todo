f2d5b8979aad412057a56e394086c594f258bf7c
Naman Jindal <namanjindal113@gmail.com>
2020-06-13 15:47:36 +0530

    initial file directory setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const db = require("./config/mongoose");
+const port = 8001;
+app.use(express.urlencoded());
+app.use(express.static('./views'));
+
+
+app.set("view engine", "ejs");
+app.set('views', './views');
+app.use('/', require('./routes/index.js'));
+app.listen(port, (err)=>{
+    if(err){
+        console.log(`There is an error: ${err}`);
+        return;
+    }
+    console.log(`The server is running on port: ${port}`);
+})
